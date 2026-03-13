d01031f7752fbb06e34b1fb8879575e62d40ed2a
DefVTans <110723419+DefVTans@users.noreply.github.com>
2026-03-13 10:46:52 +0500

    Add files via upload

---
+++ scripts/world/tilesets/Hometown_exterior_objects.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.11.2",
+  name = "Hometown_exterior_objects",
+  class = "",
+  tilewidth = 20,
+  tileheight = 20,
+  spacing = 0,
+  margin = 0,
+  columns = 60,
+  image = "../../../assets/sprites/tilesets/Hometown(exterior).png",
+  imagewidth = 1200,
+  imageheight = 3000,
+  objectalignment = "unspecified",
+  tilerendersize = "grid",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 20,
+    height = 20
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 9000,
+  tiles = {}
+}
---
+++ scripts/world/tilesets/Hometown_exterior_objects.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Hometown_exterior_objects" tilewidth="20" tileheight="20" tilecount="9000" columns="60" tilerendersize="grid">
+ <editorsettings>
+  <export target="Hometown_exterior_objects.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/Hometown(exterior).png" width="1200" height="3000"/>
+</tileset>
